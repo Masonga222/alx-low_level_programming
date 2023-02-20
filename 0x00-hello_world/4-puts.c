b5376673359fba98ce9e27b07c6ee2071377f132
Masonga222 <masongashemap@gmail>
2023-02-20 06:16:00 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
-5.Hello, printf
-#include <stdio.h>
-/**
- * main - A program that prints a line using the printf function
- * Return: 0 (Success)
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
