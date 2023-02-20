5305852006f9cb74501ec4c19ca7ca1a0de95916
Masonga222 <masongashemap@gmail>
2023-02-20 06:00:09 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+5.Hello, printf
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
