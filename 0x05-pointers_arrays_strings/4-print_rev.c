71054df54d0ea3d1897d7e85546514aae9709242
Masonga222 <masongashemap@gmail>
2023-02-27 17:35:53 -0800

    a function that prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,40 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
- *
- * Return: A integer
- */
-int _atoi(char *s)
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
 {
 	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
 
-	while (s[c])
+	while (s[c] != '\0')
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-
-		if (isi == 1)
-		{
-			break;
-		}
-
 		c++;
 	}
 
-	ni *= min;
-	return (ni);
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
 }
