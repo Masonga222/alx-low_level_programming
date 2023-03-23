0a6233e63adbf76bc90fec952188f5516528b1aa
Masonga222 <masongashemap@gmail>
2023-03-23 15:50:29 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,35 @@
 #include "variadic_functions.h"
+	#include <stdio.h>
+	#include <stdarg.h>
 
-/**
- * sum_them_all - returns the sum of all its parameters.
- * @n: amount of the arguments.
- *
- * Return: sum of its parameters.
- */
-int sum_them_all(const unsigned int n, ...)
-{
-	va_list valist;
-	unsigned int i;
-	int sum = 0;
 
-	if (n == 0)
-		return (0);
+	/**
+	 * print_numbers - Prints numbers, followed by a new line.
+	 * @separator: The string to be printed between numbers.
+	 * @n: The number of integers passed to the function.
+	 * @...: A variable number of numbers to be printed.
+	 */
+	void print_numbers(const char *separator, const unsigned int n, ...)
+	{
+		va_list nums;
+		unsigned int index;
 
-	va_start(valist, n);
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
+		va_start(nums, n);
 
-	va_end(valist);
 
-	return (sum);
-}
+		for (index = 0; index < n; index++)
+		{
+			printf("%d", va_arg(nums, int));
+
+
+			if (index != (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+
+
+		printf("\n");
+
+
+		va_end(nums);
+	}
