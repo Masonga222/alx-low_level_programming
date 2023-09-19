441b0a263274a6fda48da59157c7fa7997de354d
Masonga222 <masongashemap@gmail.com>
2023-09-18 21:56:28 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,23 @@
-include "main.h"
+nclude "main.h"
+
 
 /**
- * binary_to_uint - converts a binary to decimal
- * @b: binary val to be converted
- *
- * Return: the converted num
- */
+* binary_to_uint - The function  converts a binary number to an unsigned int
+* @b: pointing a string of the binary number(0&1)
+* Return: the converted number
+* whritten by (Fulgence nahimana)
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	int j;
-	unsigned int Sum = 0;
-
-	if (!b)
-		return (0);
-
-	for (j = 0; b[j]; j++)
-	{
-		if (b[j] < '0' || b[j] > '1')
-			return (0);
-		Sum = 2 * Sum + (b[j] - '0');
-	}
-
-	return (Sum);
+int y;
+unsigned int Myvar = 0;
+if (!b)
+return (0);
+for (y = 0; b[y]; y++)
+{
+if (b[y] < '0' || b[y] > '1')
+return (0);
+Myvar = 2 * Myvar + (b[y] - '0');
+}
+return (Myvar);
 }
