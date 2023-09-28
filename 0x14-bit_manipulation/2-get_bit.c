97a39c81608ab425f5406c5593934a54bfe1ec05
Masonga222 <masongashemap@gmail.com>
2023-09-28 08:50:08 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
+
 /**
-* get_bit - function that returns the value of a bit at a given index
-* @n: number to search
-* @index: index of the bit
-* main - check the code
-* Return: value of the bit
+* set_bit - This function sets a bit at a given index to 1
+* @n: pointer to the number to change
+* @index: index of the bit to set to 1
+*
+* Return: 1 for success, -1 for failure
 */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-int vari;
 if (index > 63)
 return (-1);
-vari= (n >> index) & 1;
-return (vari);
+*n = ((1UL << index) | *n);
+return (1);
 }
