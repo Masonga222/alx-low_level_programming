7ff3cc072ccbb6cc8d8ac5e0b482478b8e034d91
Masonga222 <masongashemap@gmail.com>
2023-11-09 11:26:10 -0800

    doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,20 @@
 #include "lists.h"
+
 /**
- * dlistint_len -Function that will return number of elements in dll
- * @h: Pointer to struct of ddl
- * Return: The amount of elements in dll
- */
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t dlistint_len(const dlistint_t *h)
 {
-        int count = 0;
+	const dlistint_t *node = h;
+	size_t cont = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                count++;
-        }
-        return (count);
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
+
+	return (cont);
 }
